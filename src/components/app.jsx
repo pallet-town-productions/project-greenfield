da85cfd85ced3f8c1ec389b78d14beb67435a58a
jbc5099 <jbc5099@gmail.com>
2019-08-22 17:18:39 -0600

    added node-sass

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../styles/standard-styles.scss';
 
 class App extends Component {
   constructor(props) {
@@ -8,7 +9,10 @@ class App extends Component {
 
   render() {
     return (
-      <div>Hello World</div>
+      <div>
+Hello World
+        <button type="button">test</button>
+      </div>
     );
   }
 }
