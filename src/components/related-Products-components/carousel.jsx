4166f30cf405e6e3290135a94da35e9d7064f4d0
jbc5099 <jbc5099@gmail.com>
2019-08-23 17:12:07 -0600

    created carousel component, still need to write tests

---
+++ src/components/related-Products-components/carousel.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PT from 'prop-types';
+import { connect } from 'react-redux';
+import '../../styles/standard-styles.scss';
+import '../../styles/related-products.scss';
+import ConnectedRelatedCard from './relatedCard';
+
+const mapStateToProps = (state) => ({
+  ...state,
+});
+
+export const Carousel = (props) => {
+  const { relatedReducer } = props;
+  return (
+    <div className="carousel-container">
+      {relatedReducer.map((related) => <ConnectedRelatedCard key={related} productId={related} />)}
+    </div>
+  );
+};
+
+Carousel.propTypes = {
+  relatedReducer: PT.arrayOf(PT.number).isRequired,
+};
+
+const connectedCarousel = connect(mapStateToProps, null)(Carousel);
+export default connectedCarousel;
