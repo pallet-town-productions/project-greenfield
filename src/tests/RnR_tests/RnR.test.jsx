e8faffa02adb715555ba85bbbd5e84556f75b5ee
Stephen Hodgetts <stephen@Stephens-MacBook-Pro.local>
2019-08-22 21:19:00 -0600

    changed filename for tests to .jsx. The tests can still be found by script and run without errors now

---
+++ src/tests/RnR_tests/RnR.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from '../../components/app';
+import RnR from '../../components/RnR/RnR_container';
+
+configure({ adapter: new Adapter() });
+
+test('it can find a div', () => {
+  const wrapper = mount(<App />);
+  expect(wrapper.find('RnR')).toBeTruthy();
+});
+
+test('it can find a header in RnR', () => {
+  const wrapper = mount(<RnR />);
+  expect(wrapper.find('H2')).toBeTruthy();
+});
