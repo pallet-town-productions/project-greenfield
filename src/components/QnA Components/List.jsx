8de82085cfed722832ea33f49b54bf8f48e9cabf
VincentCSS <VincentCSS@github.com>
2019-08-23 10:26:56 -0600

    correctly structured out list component

--- src/components/QnA Components/List.jsx
+++ src/components/QnA Components/List.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Question from './Question';
 import Answer from './Answer';
 
-const List = () => (
-  <div>
-    List Component
-    <div>
-      <Question />
-      <Answer />
-    </div>
-  </div>
-);
+class List extends React.Component {
+  constructor({ QnA }) {
+    super({ QnA });
+    this.state = {
+      QnA,
+    };
+  }
+
+  render() {
+    const { QnA } = this.state;
+    return (
+      <div>
+        {QnA.map((question) => (
+          <div>
+            {' '}
+            <Question data={question} />
+            {' '}
+            <Answer data={question} />
+            {' '}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+List.propTypes = {
+  QnA: PropTypes.arrayOf.isRequired,
+};
 
 export default List;
