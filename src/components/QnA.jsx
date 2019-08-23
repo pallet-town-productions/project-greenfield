35e69a2b5f227155ffd6fb8d2dbadb9b12d63a5b
Vincent <btheriau0421@gmail.com>
2019-08-22 21:30:31 -0600

    setup basic file structure for qna

---
+++ src/components/QnA.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Ask from './QnA Components/Ask';
+import List from './QnA Components/List';
+import Search from './QnA Components/Search';
+
+class QnA extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>QUESTIONS & ANSWERS</h3>
+        <Search />
+        <List />
+        <button type="submit">More Answered Quesitons</button>
+        <Ask />
+      </div>
+    );
+  }
+}
+
+export default QnA;
