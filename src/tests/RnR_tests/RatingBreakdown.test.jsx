3cefb3624ace0b5934bb96aa9936767bdd149fc5
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-08-23 14:10:24 -0600

    additional rating breakdown tests

--- src/tests/RnR_tests/RatingBreakdown.test.jsx
+++ src/tests/RnR_tests/RatingBreakdown.test.jsx
@@ -5,7 +5,6 @@ import RatingBreakdown from '../../components/RnR/RnR_RatingBreakdown';
 
 configure({ adapter: new Adapter() });
 
-// Tests for RnR
 function testRatingBreakdown() {
   const props = {
   };
@@ -17,9 +16,16 @@ function testRatingBreakdown() {
 }
 
 describe('RnR - Rating Breakdown', () => {
+  const { enzymeWrapper } = testRatingBreakdown();
   it('should have an ul element with 5 li elements for star breakdown', () => {
-    const { enzymeWrapper } = testRatingBreakdown();
     expect((enzymeWrapper.find('ul').length)).toBe(1);
     expect((enzymeWrapper.find('li').length)).toBe(5);
+  });
+
+  it('should have proper classes assigned to elements', () => {
+    expect(enzymeWrapper.find('h2').hasClass('rating-average')).toBe(true);
+    expect(enzymeWrapper.find('span').hasClass('star-rating')).toBe(true);
+    expect(enzymeWrapper.find('p').hasClass('percent-recommended')).toBe(true);
+    expect(enzymeWrapper.find('ul').hasClass('star-breakdown')).toBe(true);
   });
 });
