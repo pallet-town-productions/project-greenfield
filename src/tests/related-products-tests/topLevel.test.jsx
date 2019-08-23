c4b0f63d8ea0780e973c36f6f365a6b593452fe1
jbc5099 <jbc5099@gmail.com>
2019-08-23 12:23:40 -0600

    added basic test for top level related products component

---
+++ src/tests/related-products-tests/topLevel.test.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { RelatedProducts } from '../../components/related-Products-components/related-Products';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+function setup() {
+  const props = {
+    productId: 1,
+  };
+
+  const enzymeWrapper = shallow(<RelatedProducts {...props} />);
+
+  return {
+    props,
+    enzymeWrapper,
+  };
+}
+
+describe('components', () => {
+  describe('Related products', () => {
+    it('should render self and subcomponents', () => {
+      const { enzymeWrapper } = setup();
+
+      expect(enzymeWrapper.find('div').hasClass('related-products-container')).toBe(true);
+
+      expect(enzymeWrapper.find('div').text()).toBe('1');
+    });
+  });
+});
