45af7e55147944ba8290f206893efcd6476228cd
Han Yan <hanprogramming@gmail.com>
2019-08-24 14:05:48 -0600

    Linted price.jsx

--- src/components/overview-components/productInformation/price.jsx
+++ src/components/overview-components/productInformation/price.jsx
@@ -6,23 +6,27 @@ import '../../../styles/overview.scss';
 const mapStateToProps = function (state) {
   const { productData, style, currentStyleIndex } = state;
   return { productData, style, currentStyleIndex };
-}
+};
 
 const Price = function ({ productData, style, currentStyleIndex }) {
-  let price = Number(style.results[currentStyleIndex].original_price) || Number(productData.default_price);
-  let salePrice = Number(style.results[currentStyleIndex].sale_price); // default 0
+  const price = Number(style.results[currentStyleIndex].original_price)
+    || Number(productData.default_price);
+  const salePrice = Number(style.results[currentStyleIndex].sale_price); // default 0
 
   if (salePrice) {
-    return <div>
-      <span className="price" id="sale-price">{`$${salePrice}`}</span>
-      <span className="price" id="original-price">{`$${price}`}</span>
-    </div>
-  } else {
-    return <div>
+    return (
+      <div>
+        <span className="price" id="sale-price">{`$${salePrice}`}</span>
+        <span className="price" id="original-price">{`$${price}`}</span>
+      </div>
+    );
+  }
+  return (
+    <div>
       <span className="price">{`$${price}`}</span>
     </div>
-  }
-}
+  );
+};
 
 Price.propTypes = {
   productData: PT.shape({
@@ -32,7 +36,7 @@ Price.propTypes = {
     results: PT.arrayOf(PT.shape({
       original_price: PT.string.isRequired, // sketchy!!
       sale_price: PT.string.isRequired, // sketchy!!
-    }))
+    })),
   }).isRequired,
   currentStyleIndex: PT.number.isRequired,
 };
