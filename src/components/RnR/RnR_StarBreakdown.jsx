2a0aa02399a69890a4251dc9e258e58b707a167f
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-08-24 11:37:28 -0600

    star breakdown logic added with test data

--- src/components/RnR/RnR_StarBreakdown.jsx
+++ src/components/RnR/RnR_StarBreakdown.jsx
@@ -1,28 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const StarBreakdown = () => (
-  <ul className="star-breakdown">
-    <li>
-      <a href="google.com">5 stars</a>
-      <progress value="70" max="100" />
-    </li>
-    <li>
-      <a href="google.com">4 stars</a>
-      <progress value="90" max="100" />
-    </li>
-    <li>
-      <a href="google.com">3 stars</a>
-      <progress value="60" max="100" />
-    </li>
-    <li>
-      <a href="google.com">2 stars</a>
-      <progress value="30" max="100" />
-    </li>
-    <li>
-      <a href="google.com">1 stars</a>
-      <progress value="40" max="100" />
-    </li>
-  </ul>
-);
+
+const StarBreakdown = ({ ratings }) => {
+  const defaultRatings = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+  };
+  const allRatings = { ...defaultRatings, ...ratings };
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  const totalRatings = Object.values(allRatings)
+    .reduce(reducer);
+
+  return (
+    <ul className="star-breakdown">
+      <li>
+        <a href="google.com">5 stars</a>
+        <progress value={allRatings[5]} max={totalRatings} />
+      </li>
+      <li>
+        <a href="google.com">4 stars</a>
+        <progress value={allRatings[4]} max={totalRatings} />
+      </li>
+      <li>
+        <a href="google.com">3 stars</a>
+        <progress value={allRatings[3]} max={totalRatings} />
+      </li>
+      <li>
+        <a href="google.com">2 stars</a>
+        <progress value={allRatings[2]} max={totalRatings} />
+      </li>
+      <li>
+        <a href="google.com">1 stars</a>
+        <progress value={allRatings[1]} max={totalRatings} />
+      </li>
+    </ul>
+  );
+};
+
+
+StarBreakdown.propTypes = {
+  ratings: PropTypes.shape({}).isRequired,
+};
+
 
 export default StarBreakdown;
