112a466b0cdae99bb4450e359dbb6f2962146537
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-08-23 19:38:09 -0600

    dynamic star render w updated  tests, fixed react component error for RatingBreakdown

---
+++ src/tests/RnR_tests/StarRating.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import StarRating from '../../components/RnR/RnR_StarRating';
+
+configure({ adapter: new Adapter() });
+
+function testStarRating(starCount) {
+  const props = {
+  };
+  const enzymeWrapper = shallow(<StarRating starCount={starCount} />);
+  return {
+    props,
+    enzymeWrapper,
+  };
+}
+
+describe('RnR - Star Rating', () => {
+  it('should have 5 i elements representing stars for whole number ratings', () => {
+    const { enzymeWrapper } = testStarRating(4);
+    expect((enzymeWrapper.find('i').length)).toBe(5);
+  });
+
+  it('should have 5 i elements representing stars for decimal number ratings', () => {
+    const { enzymeWrapper } = testStarRating(4.5);
+    expect((enzymeWrapper.find('i').length)).toBe(5);
+  });
+
+  it('should not render stars if rating is greater than 5', () => {
+    const { enzymeWrapper } = testStarRating(7);
+    expect((enzymeWrapper.find('i').length)).toBe(0);
+  });
+
+  it('should not render stars if rating is less than 0', () => {
+    const { enzymeWrapper } = testStarRating(-1);
+    expect((enzymeWrapper.find('i').length)).toBe(0);
+  });
+});
