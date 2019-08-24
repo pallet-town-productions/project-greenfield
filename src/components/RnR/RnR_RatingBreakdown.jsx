e6a5330ae5c8c6f9ca5048792c6f5443283e1c70
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-08-24 17:24:27 -0600

    fixed component naming problem

--- src/components/RnR/RnR_RatingBreakdown.jsx
+++ src/components/RnR/RnR_RatingBreakdown.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import StarRating from './RnR_StarRating';
-import StarBreakdown from './RnR_StarBreakdown';
+import ConnectedStarBreakdown from './RnR_StarBreakdown';
 
 const RatingBreakdown = () => (
   <div>
@@ -9,7 +9,7 @@ const RatingBreakdown = () => (
       <StarRating starCount={3.5} />
     </h2>
     <p className="percent-recommended">100% of reviews recommend this product</p>
-    <StarBreakdown ratings={{ 1: 3, 4: 5 }} />
+    <ConnectedStarBreakdown ratings={{ 1: 3, 4: 5 }} />
   </div>
 );
 
