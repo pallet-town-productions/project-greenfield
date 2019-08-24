c1f71b10cd31ee7964eb7cb4e599beafc6206014
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-08-24 10:37:53 -0600

    star breakdown component added

--- src/components/RnR/RnR_RatingBreakdown.jsx
+++ src/components/RnR/RnR_RatingBreakdown.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import StarRating from './RnR_StarRating';
+import StarBreakdown from './RnR_StarBreakdown';
 
 class RatingBreakdown extends Component {
   constructor(props) {
@@ -16,28 +17,7 @@ class RatingBreakdown extends Component {
           <StarRating starCount={3.5} />
         </h2>
         <p className="percent-recommended">100% of reviews recommend this product</p>
-        <ul className="star-breakdown">
-          <li>
-            <a href="google.com">5 stars</a>
-            <progress value="70" max="100" />
-          </li>
-          <li>
-            <a href="google.com">4 stars</a>
-            <progress value="90" max="100" />
-          </li>
-          <li>
-            <a href="google.com">3 stars</a>
-            <progress value="60" max="100" />
-          </li>
-          <li>
-            <a href="google.com">2 stars</a>
-            <progress value="30" max="100" />
-          </li>
-          <li>
-            <a href="google.com">1 stars</a>
-            <progress value="40" max="100" />
-          </li>
-        </ul>
+        <StarBreakdown />
       </div>
     );
   }
