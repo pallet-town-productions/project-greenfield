71e406c9a24eb9575abc7df868ade84436bfe58e
Han Yan <hanprogramming@gmail.com>
2019-08-24 14:00:30 -0600

    Added Dynamic Price

---
+++ src/components/overview-components/productInformation/price.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PT from 'prop-types';
+import { connect } from 'react-redux';
+import '../../../styles/overview.scss';
+
+const mapStateToProps = function (state) {
+  const { productData, style, currentStyleIndex } = state;
+  return { productData, style, currentStyleIndex };
+}
+
+const Price = function ({ productData, style, currentStyleIndex }) {
+  let price = Number(style.results[currentStyleIndex].original_price) || Number(productData.default_price);
+  let salePrice = Number(style.results[currentStyleIndex].sale_price); // default 0
+
+  if (salePrice) {
+    return <div>
+      <span className="price" id="sale-price">{`$${salePrice}`}</span>
+      <span className="price" id="original-price">{`$${price}`}</span>
+    </div>
+  } else {
+    return <div>
+      <span className="price">{`$${price}`}</span>
+    </div>
+  }
+}
+
+Price.propTypes = {
+  productData: PT.shape({
+    default_price: PT.string.isRequired, // sketchy!!
+  }).isRequired,
+  style: PT.shape({
+    results: PT.arrayOf(PT.shape({
+      original_price: PT.string.isRequired, // sketchy!!
+      sale_price: PT.string.isRequired, // sketchy!!
+    }))
+  }).isRequired,
+  currentStyleIndex: PT.number.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Price);
