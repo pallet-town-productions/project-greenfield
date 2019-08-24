831c6b7ebbd1be6ad897d91965f2f08a6ebb4374
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-08-24 15:11:19 -0600

    connected components

--- src/components/RnR/RnR_RatingBreakdown.jsx
+++ src/components/RnR/RnR_RatingBreakdown.jsx
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import StarRating from './RnR_StarRating';
 import StarBreakdown from './RnR_StarBreakdown';
 
-class RatingBreakdown extends Component {
-  constructor(props) {
-    super(props);
+const mapStateToProps = (state) => ({
+  ...state,
+});
 
-    this.state = {};
-  }
+const RatingBreakdown = () => (
+  <div>
+    <h2 className="rating-average">
+      3.5
+      <StarRating starCount={3.5} />
+    </h2>
+    <p className="percent-recommended">100% of reviews recommend this product</p>
+    <StarBreakdown ratings={{ 1: 3, 4: 5 }} />
+  </div>
+);
 
-  render() {
-    return (
-      <div>
-        <h2 className="rating-average">
-          3.5
-          <StarRating starCount={3.5} />
-        </h2>
-        <p className="percent-recommended">100% of reviews recommend this product</p>
-        <StarBreakdown ratings={{ 1: 5, 4: 7 }} />
-      </div>
-    );
-  }
-}
-
-export default RatingBreakdown;
+const connectedRatingBreakdown = connect(mapStateToProps, null)(RatingBreakdown);
+export default connectedRatingBreakdown;
