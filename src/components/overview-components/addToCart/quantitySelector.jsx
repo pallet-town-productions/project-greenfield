b053ebf2d94c8b40ffd170fb19d9e738a8479417
Han Yan <hanprogramming@gmail.com>
2019-08-24 18:39:05 -0600

    Added quantitySelector

---
+++ src/components/overview-components/addToCart/quantitySelector.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PT from 'prop-types';
+import { connect } from 'react-redux';
+import { range } from 'underscore';
+import { changeQuantity } from '../../../actions/overview-Actions/addToCart/changeSizeQty';
+
+const BLANKQUANTITY = '-';
+
+const mapStateToProps = function (state) {
+  const { currentSizeIndex, showQuantities } = state; // this is for detecting if Size is selected
+  const currentAvailQuantity = 20; // dummy for future additional functionality
+  return {
+    currentSizeIndex,
+    currentAvailQuantity,
+    showQuantities,
+  };
+};
+
+const mapDispatchToProps = function (dispatch) {
+  return {
+    handleQuantityChange: () => {
+      const selectedQuantity = document.getElementById('current-quantity').value;
+      dispatch(changeQuantity(selectedQuantity));
+    },
+  };
+};
+
+const QuantitySelector = function ({ showQuantities, currentAvailQuantity, handleQuantityChange }) {
+  const quantityList = range(1, Math.min(currentAvailQuantity, 15) + 1);
+  const list = (showQuantities) ? quantityList : [BLANKQUANTITY];
+  return (
+    <div id="quantity-selector" className="dropdown-selector">
+      <select
+        onChange={() => {
+          handleQuantityChange();
+        }}
+        id="current-quantity"
+      >
+        {list.map((qty) => (
+          <option
+            key={qty}
+            value={qty}
+          >
+            {qty}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+QuantitySelector.propTypes = {
+  showQuantities: PT.bool.isRequired,
+  currentAvailQuantity: PT.number.isRequired,
+  handleQuantityChange: PT.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuantitySelector);
