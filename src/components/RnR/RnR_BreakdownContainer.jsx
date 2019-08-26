3ae186779d3477752a4b0a1dbadb4ca9a2127389
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-08-26 13:45:49 -0600

    created container for review and product breakdown

---
+++ src/components/RnR/RnR_BreakdownContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RatingBreakdown from './RnR_RatingBreakdown';
+import ProductBreakdown from './RnR_ProductBreakdown';
+
+const BreakdownContainer = () => (
+  <div className="breakdown-container">
+    <RatingBreakdown />
+    <ProductBreakdown />
+  </div>
+);
+
+export default BreakdownContainer;
