867470ac245d09e6358068b7832aff88e0c039ea
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-08-26 14:38:06 -0600

    classnames and styling refactor

--- src/components/RnR/RnR_ProductBreakdown.jsx
+++ src/components/RnR/RnR_ProductBreakdown.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const ProductBreakdown = () => (
-  <div className="product-breakdown">
-    Product Breakdown
+  <div className="product-breakdown breakdown-widget">
+    <ul className="breakdown-list">
+      Label
+      <li className="product-breakdown-item">
+        <progress />
+        <progress />
+        <progress />
+      </li>
+    </ul>
   </div>
 );
 
