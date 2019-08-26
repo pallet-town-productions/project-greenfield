d9eaf123870f0095368a0271a630c0233cf71ad1
han-yan-ds <hanprogramming@gmail.com>
2019-08-26 11:51:50 -0600

    styleSelector.jsx had a conflict with recent merge, fixed

--- src/components/overview-components/styleSelector/styleSelector.jsx
+++ src/components/overview-components/styleSelector/styleSelector.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PT from 'prop-types';
 import { connect } from 'react-redux';
 import changeStyle from '../../../actions/overview-Actions/styleSelector/changeStyle';
-import changePhoto from '../../../actions/overview-Actions/imageGallery/changePhoto';
 import StyleThumbnail from './styleThumbnail';
 
 const mapStateToProps = function (state) {
@@ -18,7 +17,6 @@ const mapDispatchToProps = function (dispatch) {
   return {
     handleSwitchStyle: (styleIndex) => {
       dispatch(changeStyle(styleIndex));
-      dispatch(changePhoto(0)); // reset photo to first one upon each style switch
     },
   };
 };
