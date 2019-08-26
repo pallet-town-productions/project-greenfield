23c79a190c823a6a24bb53a23ebe78dfc711f55d
Stephen Hodgetts <stephen@Stephens-MacBook-Pro.local>
2019-08-26 14:41:37 -0600

    completed helpfulness once function

--- src/components/RnR/RnR_tile.jsx
+++ src/components/RnR/RnR_tile.jsx
@@ -9,6 +9,7 @@ class Tile extends React.Component {
     const { review } = this.props;
     this.state = {
       body: review.body.slice(0, 250),
+      hasVotedHelpfulness: false,
     };
   }
 
@@ -17,11 +18,27 @@ class Tile extends React.Component {
     this.setState(() => ({ body: review.body }));
   }
 
+  markAsHelpful() {
+    const { hasVotedHelpfulness } = this.state;
+    if (!hasVotedHelpfulness) {
+      const { review } = this.props;
+      // eslint-disable-next-line camelcase
+      const { review_id } = review;
+      // eslint-disable-next-line camelcase
+      return fetch(`http://18.217.220.129/reviews/helpful/${review_id}`, {
+        method: 'PUT',
+      })
+        .then(() => this.setState(() => ({ hasVotedHelpfulness: true })));
+    }
+    return 'done';
+  }
+
   render() {
     const { review } = this.props;
     const images = review.photos.map((photo) => (<img className="thumbnail" src={photo.url} alt="" />)).slice(0, 5);
     const summary = review.summary.slice(0, 60);
     let { body } = this.state;
+
     if (body.length > 249) {
       body = (
         <div>
@@ -29,13 +46,13 @@ class Tile extends React.Component {
           <button type="button" onClick={this.showFullBody.bind(this)}>Show more</button>
         </div>
       );
-    } else {
-      body = <p className="tile-body">{body.slice(0, 1000)}</p>;
-    }
+    } else { body = <p className="tile-body">{body.slice(0, 1000)}</p>; }
+
     let recommend;
     if (review.recommend) {
       recommend = <p className="recommend">I recommend this product</p>;
     }
+
     return (
       <div>
         <p>{review.rating}</p>
@@ -47,13 +64,13 @@ class Tile extends React.Component {
         {body}
         {recommend}
         <p>{images}</p>
-        <p>
-          Helpful?
+        <div className="helpful">
+          <p>Helpful?</p>
+          <div className="helpful-link" onClick={this.markAsHelpful.bind(this)} onKeyUp={this.markAsHelpful.bind(this)} tabIndex={0} role="link">Yes</div>
           (
           {review.helpfulness}
           )
-        </p>
-        <p>Report</p>
+        </div>
       </div>
     );
   }
@@ -70,7 +87,9 @@ Tile.propTypes = {
     body: PT.string,
     helpfulness: PT.number,
     recommend: PT.number,
+    review_id: PT.number,
   }).isRequired,
+  // productId: PT.number.isRequired,
 };
 
 export default Tile;
