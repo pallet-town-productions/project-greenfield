d4fdac0ae709a988bdfbfe7ba805d395db03bea9
Han Yan <hanprogramming@gmail.com>
2019-08-27 14:44:09 -0600

    quick commit, added socialMedia.jsx

---
+++ src/components/overview-components/productInformation/socialMedia.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PT from 'prop-types';
+import { connect } from 'react-redux';
+
+const SocialMediaButtons = function ({}) {
+  return (
+    
+  );
+}
+
+export default connect(null, null)(SocialMediaButtons);
