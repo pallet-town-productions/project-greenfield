89edaba6ec215c5168355cadf1ba6a8a206ccd0c
VincentCSS <VincentCSS@github.com>
2019-08-27 15:55:21 -0600

    rewrote Question component to a class component to have access to state if needed

--- src/components/QnA-components/Question.jsx
+++ src/components/QnA-components/Question.jsx
@@ -3,56 +3,76 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AddAnswer from './AddAnswer';
 
-const Question = ({
-  data,
-  helpfulClickHandler,
-  reportClickHandler,
-  productName,
-}) => (
-  <div className="questionsQuestionContainer">
-    <p>
-      {`Q: ${data.question_body}`}
-    </p>
-    <span className="questionsQuestionTools">
-      <p id={`Q${data.question_id}`}>
-        Helpful?
-        <button className="questionsHelpfulBtn" type="submit" onClick={() => helpfulClickHandler('qa', data.question_id, 'question')}>
-          <u>
-            Yes
-          </u>
-        </button>
-        (
-        {data.question_helpfulness}
-        )
-        |
-        <AddAnswer data={data} productName={productName} />
-      </p>
-    </span>
-    <div>
-      {Object.values(data.answers).slice(0, 2).map((answer) => (
-        <span className="questionsAnswerTools">
-          <p>
-            {Object.values(data.answers)[0] === answer ? `A: ${answer.body}` : answer.body}
-          </p>
-          {answer.photos.length === 0 ? '' : answer.photos.map((photo) => (
-            <img alt={`Uploaded by: ${answer.answerer_name}`} src={photo} className="thumbnail" />
-          ))}
-          <p>
-            {`by ${answer.answerer_name}, ${new Date(answer.date).toLocaleDateString()} | Helpful? `}
-            <button className="questionsHelpfulBtn" type="submit" onClick={() => helpfulClickHandler('qa', answer.id, 'answer')}>
-              <u>Yes</u>
+
+class Question extends React.Component {
+  constructor({
+    data,
+    helpfulClickHandler,
+    reportClickHandler,
+    productName,
+  }) {
+    super({
+      data,
+      helpfulClickHandler,
+      reportClickHandler,
+      productName,
+    });
+  }
+
+  render() {
+    const {
+      data,
+      productName,
+      reportClickHandler,
+      helpfulClickHandler,
+    } = this.props;
+    return (
+      <div className="questionsQuestionContainer">
+        <p>
+          {`Q: ${data.question_body}`}
+        </p>
+        <span className="questionsQuestionTools">
+          <p id={`Q${data.question_id}`}>
+            Helpful?
+            <button className="questionsHelpfulBtn" type="submit" onClick={() => helpfulClickHandler('qa', data.question_id, 'question')}>
+              <u>
+                Yes
+              </u>
             </button>
-            {` (${answer.helpfulness}) | `}
-            <button className="questionsReportBtn" type="submit" onClick={() => reportClickHandler('qa', answer.id, 'answer')}>
-              <u>Report</u>
-            </button>
+            (
+            {data.question_helpfulness}
+            )
+            |
+            <AddAnswer data={data} productName={productName} />
           </p>
-          {Object.keys(data.answers).length > 2 && Object.values(data.answers)[0] !== answer ? <button type="button">Load More Answers...</button> : ''}
         </span>
-      ))}
-    </div>
-  </div>
-);
+        <div>
+          {Object.values(data.answers).slice(0, 2).map((answer) => (
+            <span className="questionsAnswerTools">
+              <p>
+                {Object.values(data.answers)[0] === answer ? `A: ${answer.body}` : answer.body}
+              </p>
+              {answer.photos.length === 0 ? '' : answer.photos.map((photo) => (
+                <img alt={`Uploaded by: ${answer.answerer_name}`} src={photo} className="thumbnail" />
+              ))}
+              <p>
+                {`by ${answer.answerer_name}, ${new Date(answer.date).toLocaleDateString()} | Helpful? `}
+                <button className="questionsHelpfulBtn" type="submit" onClick={() => helpfulClickHandler('qa', answer.id, 'answer')}>
+                  <u>Yes</u>
+                </button>
+                {` (${answer.helpfulness}) | `}
+                <button className="questionsReportBtn" type="submit" onClick={() => reportClickHandler('qa', answer.id, 'answer')}>
+                  <u>Report</u>
+                </button>
+              </p>
+              {Object.keys(data.answers).length > 2 && Object.values(data.answers)[0] !== answer ? <button type="button">Load More Answers...</button> : ''}
+            </span>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 Question.propTypes = {
   data: PropTypes.shape({
