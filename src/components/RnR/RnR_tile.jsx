f39fcad5ddc4ec7869be6ba3b3cc2c750b3d337c
Stephen Hodgetts <stephen@Stephens-MacBook-Pro.local>
2019-08-28 11:33:18 -0600

    completed refactor of lower bar

--- src/components/RnR/RnR_tile.jsx
+++ src/components/RnR/RnR_tile.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import PT from 'prop-types';
 import '../../styles/standard-styles.scss';
@@ -5,103 +6,53 @@ import '../../styles/RnR-styles.scss';
 import StarRating from './RnR_StarRating';
 import PhotoThumbnails from './RnR_tile_photos';
 import TileBody from './RnR_tile_body';
+import LowerBar from './RnR_tile_lower_bar';
 
 
-class Tile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasVotedHelpfulness: false,
-      hasReported: false,
-    };
-  }
+const Tile = (props) => {
 
-  markAsHelpful() {
-    const { hasVotedHelpfulness } = this.state;
-    if (!hasVotedHelpfulness) {
-      const { review } = this.props;
-      // eslint-disable-next-line camelcase
-      const { review_id } = review;
-      // eslint-disable-next-line camelcase
-      return fetch(`http://18.217.220.129/reviews/helpful/${review_id}`, {
-        method: 'PUT',
-      })
-        .then(() => this.setState(() => ({ hasVotedHelpfulness: true })));
-    }
-    return 'done';
-  }
+  const { review } = props;
+  const { body } = review;
+  let { response } = review;
+  const summary = review.summary.slice(0, 60);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
 
-  report() {
-    const { hasReported } = this.state;
-    if (!hasReported) {
-      const { review } = this.props;
-      // eslint-disable-next-line camelcase
-      const { review_id } = review;
-      // eslint-disable-next-line camelcase
-      return fetch(`http://18.217.220.129/reviews/report/${review_id}`, {
-        method: 'PUT',
-      })
-        .then(() => this.setState(() => ({ hasReported: true })));
-    }
-    return 'done';
-  }
+  const photos = review.photos.map((photo) => (
+    <PhotoThumbnails photo={photo} />
+  ));
 
-  render() {
-    const { review } = this.props;
-    let { body } = review;
-    let { response } = review;
-    const summary = review.summary.slice(0, 60);
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-
-    const photos = review.photos.map((photo) => (
-      <PhotoThumbnails photo={photo} />
-    ));
-
-    if (review.response !== 'null') {
-      response = (
-        <div className="response">
-          <p className="response-text-header">Response</p>
-          <p className="response-text">{review.response}</p>
-        </div>
-      );
-    } else {
-      response = <div />;
-    }
-
-    let recommend;
-    if (review.recommend) {
-      recommend = <p className="recommend">&#10004; I recommend this product</p>;
-    }
-
-    return (
-      <div className="tile">
-        <div className="top-row">
-          <StarRating className="star-rating" starCount={review.rating} />
-          <p className="user">{new Date(review.date).toLocaleDateString('en-US', options)}</p>
-          <p className="user">
-            {review.reviewer_name}
-            ,
-          </p>
-        </div>
-        <p className="summary">{summary}</p>
-        <div className="body"><TileBody body={body}/></div>
-        <div className="recommend">{recommend}</div>
-        <div>{response}</div>
-        <div className="photos">{photos}</div>
-        <div className="lower-row">
-          <p className="lower">Helpful?</p>
-          <div className="lower test" onClick={this.markAsHelpful.bind(this)} onKeyUp={this.markAsHelpful.bind(this)} tabIndex={0} role="link">Yes</div>
-          <div className="lower helpfulness">
-            (
-            {review.helpfulness}
-            )
-          </div>
-          <div className="lower test report" onClick={this.report.bind(this)} onKeyDown={this.report.bind(this)} tabIndex={0} role="link">Report</div>
-        </div>
+  if (review.response !== 'null') {
+    response = (
+      <div className="response">
+        <p className="response-text-header">Response</p>
+        <p className="response-text">{review.response}</p>
       </div>
     );
+  } else {
+    response = <div />;
   }
-}
+
+  let recommend;
+  if (review.recommend) {
+    recommend = <p className="recommend">&#10004; I recommend this product</p>;
+  }
+
+  return (
+    <div className="tile">
+      <div className="top-row">
+        <StarRating className="star-rating" starCount={review.rating} />
+        <p className="user">{new Date(review.date).toLocaleDateString('en-US', options)}</p>
+        <p className="user">{review.reviewer_name},</p>
+      </div>
+      <p className="summary">{summary}</p>
+      <div className="body"><TileBody body={body} /></div>
+      <div className="recommend">{recommend}</div>
+      <div>{response}</div>
+      <div className="photos">{photos}</div>
+      <LowerBar review={review} />
+    </div>
+  );
+};
 
 Tile.propTypes = {
   review: PT.shape({
@@ -113,9 +64,7 @@ Tile.propTypes = {
     reviewer_name: PT.string,
     summary: PT.string,
     body: PT.string,
-    helpfulness: PT.number,
     recommend: PT.number,
-    review_id: PT.number,
   }).isRequired,
 };
 
---
+++ src/components/RnR/RnR_tile_lower_bar.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+import PT from 'prop-types';
+import '../../styles/standard-styles.scss';
+import '../../styles/RnR-styles.scss';
+
+class LowerBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasVotedHelpfulness: false,
+      hasReported: false,
+    };
+  }
+
+  markHelpful() {
+    const { hasVotedHelpfulness } = this.state;
+    if (!hasVotedHelpfulness) {
+      const { review } = this.props;
+      // eslint-disable-next-line camelcase
+      const { review_id } = review;
+      // eslint-disable-next-line camelcase
+      return fetch(`http://18.217.220.129/reviews/helpful/${review_id}`, {
+        method: 'PUT',
+      })
+        .then(() => this.setState(() => ({ hasVotedHelpfulness: true })));
+    }
+    return 'done';
+  }
+
+  report() {
+    const { hasReported } = this.state;
+    if (!hasReported) {
+      const { review } = this.props;
+      // eslint-disable-next-line camelcase
+      const { review_id } = review;
+      // eslint-disable-next-line camelcase
+      return fetch(`http://18.217.220.129/reviews/report/${review_id}`, {
+        method: 'PUT',
+      })
+        .then(() => this.setState(() => ({ hasReported: true })))
+        .then(() => alert('Thank you for your feedback'));
+    }
+    return 'done';
+  }
+
+  render() {
+    const { review } = this.props;
+    return (
+      <div className="lower-row">
+        <p className="lower">Helpful?</p>
+        <div className="lower test" onClick={this.markHelpful.bind(this)} onKeyUp={this.markHelpful.bind(this)} tabIndex={0} role="link">Yes</div>
+        <div className="lower helpfulness">
+          ({review.helpfulness})
+        </div>
+        <div className="lower test report" onClick={this.report.bind(this)} onKeyDown={this.report.bind(this)} tabIndex={0} role="link">Report</div>
+      </div>
+    );
+  }
+}
+
+LowerBar.propTypes = {
+  review: PT.shape({
+    helpfulness: PT.number.isRequired,
+    review_id: PT.number.isRequired,
+  }).isRequired,
+};
+
+export default LowerBar;
