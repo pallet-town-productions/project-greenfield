94fa905090b405c2b2ac584ae1b69355dd72fe86
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-08-29 13:28:08 -0600

    import review-meta utils

--- src/components/RnR/RnR_ProductBreakdown.jsx
+++ src/components/RnR/RnR_ProductBreakdown.jsx
@@ -1,82 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { setProductRatingValue, setProductSublables } from '../../util/RnR-review-meta';
 
+// TODO: refactor to not map entire state to props
 const mapStateToProps = (state) => ({
   ...state,
 });
 
-// Function to set value for characteristic rating scale represented by progress bar
-const setProductRatingValue = (value = 0) => {
-  const values = {
-    leftValue: 0,
-    centerValue: 0,
-    rightValue: 0,
-  };
-  if (value > 0 && value < 1.5) {
-    values.leftValue = value;
-  }
-  if (value >= 1.5 && value < 3) {
-    values.centerValue = value;
-  }
-  if (value >= 3 && value <= 5) {
-    values.rightValue = value;
-  }
-  return values;
-};
-
-// Function to set characteristic sublabels for rating scale
-const setSublables = (label = 'Default') => {
-  const defaultSubLabels = {
-    Size: {
-      left: 'A size too small',
-      center: 'Perfect',
-      right: 'A size too big',
-    },
-    Width: {
-      left: 'Too narrow',
-      center: 'Perfect',
-      right: 'Too wide',
-    },
-    Comfort: {
-      left: 'Uncomfortable',
-      center: 'Ok',
-      right: 'Perfect',
-    },
-    Quality: {
-      left: 'Poor',
-      center: 'What I expected',
-      right: 'Perfect',
-    },
-    Length: {
-      left: 'Runs short',
-      center: 'Perfect',
-      right: 'Runs long',
-    },
-    Fit: {
-      left: 'Runs tight',
-      center: 'Perfect',
-      right: 'Runs loose',
-    },
-    Default: {
-      left: 'Not enough',
-      center: 'Perfect',
-      right: 'Too much',
-    },
-  };
-  return {
-    leftSublabel: defaultSubLabels[label].left || defaultSubLabels.Default.left,
-    centerSublabel: defaultSubLabels[label].center || defaultSubLabels.Default.center,
-    rightSublabel: defaultSubLabels[label].right || defaultSubLabels.Default.right,
-  };
-};
-
-// Functional Component
 const ProductBreakdown = ({ getMetaData }) => {
   const { characteristics } = getMetaData;
   const productLabels = Object.keys(characteristics || [])
     .map((label) => {
-      const { leftSublabel, centerSublabel, rightSublabel } = setSublables(label);
+      const { leftSublabel, centerSublabel, rightSublabel } = setProductSublables(label);
       const {
         leftValue,
         centerValue,
