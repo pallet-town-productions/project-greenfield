4d5dfe4cbb249eb9ec52e6ef08482baab3a689e0
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-08-30 13:51:44 -0600

    inputs outlined

--- src/components/RnR/RnR_WriteReview.jsx
+++ src/components/RnR/RnR_WriteReview.jsx
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
 
 class WriteReview extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      overallRating: '',
-      recommended: '',
-      characteristics: '',
+      overallRating: true,
+      recommended: true,
+      characteristics: false,
       reviewSummary: '',
       reviewBody: '',
       photos: '',
       nickname: '',
-      email: ''
+      email: '',
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -31,23 +30,112 @@ class WriteReview extends Component {
   handleSubmit(event) {
     event.preventDefault();
     const formData = this.state;
+    console.log(formData);
+    // check if data is valid
     // make a post with form data if everything all good
+    // otherwise render some errors
   }
 
   render() {
+    const {
+      overallRating,
+      recommended,
+      characteristics,
+      reviewSummary,
+      reviewBody,
+      photos,
+      nickname,
+      email,
+    } = this.state;
     return (
       <form>
-        <label htmlFor="default">
-          Default Value
+        <label htmlFor="overallRating">
+          Overall Rating
           <input
-            id="default"
-            name=""
-            type=""
-            value=""
+            id="overallRating"
+            name="overallRating"
+            type="radio"
+            value={overallRating}
             onChange={this.handleInputChange}
           />
         </label>
         <br />
+        <label htmlFor="recommended">
+        Recommended
+          <input
+            id="recommended"
+            name="recommended"
+            type="radio"
+            value={recommended}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="characteristics">
+        Characteristics
+          <input
+            id="characteristics"
+            name="characteristics"
+            type="radio"
+            value={characteristics}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="reviewSummary">
+        reviewSummary
+          <input
+            id="reviewSummary"
+            name="reviewSummary"
+            type="text"
+            value={reviewSummary}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="reviewBody">
+        reviewBody
+          <input
+            id="reviewBody"
+            name="reviewBody"
+            type="textarea"
+            value={reviewBody}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="photos">
+        Recommended
+          <input
+            id="photos"
+            name="photos"
+            type="text"
+            value={photos}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="nickname">
+        nickname
+          <input
+            id="nickname"
+            name="nickname"
+            type="text"
+            value={nickname}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="email">
+        email
+          <input
+            id="email"
+            name="email"
+            type="text"
+            value={email}
+            onChange={this.handleInputChange}
+          />
+        </label>
       </form>
     );
   }
