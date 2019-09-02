5bdb119e676816189d52949fa0ad45da286a9ceb
han-yan-ds <hanprogramming@gmail.com>
2019-09-02 11:50:23 -0600

    Added get-request and parsers to util.js

--- src/util/util.js
+++ src/util/util.js
@@ -3,9 +3,41 @@ const zeroPad = function (num, places) {
   return `O${Array(+(zero > 0 && zero)).join('0')}${num}`;
 };
 
-const dummy = 'DUMMY TO CALM LINTER';
+const getData = function (endpoint, callback = () => {}) {
+  return fetch(`http://54.213.200.113:3000${endpoint}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+};
+
+const getProductData = function (productId = 1, callback = () => {}) {
+  // returns the fetch's returned promise
+  return getData(`/products/${productId}`, callback);
+};
+
+const getStyleData = function (productId = 1, callback = () => {}) {
+  // returns the fetch's returned promise
+  return getData(`/products/${productId}/styles`, callback);
+};
+
+const getProductId = function (endpoint) {
+  // this is written to extract the first instance of /number/ and return the number
+    // takes an endpoint as an argument
+    // returns product ID
+  try {
+    let match = endpoint.match(/\/\d{1,}\//)[0];
+    return Number(match.substring(1, match.length-1));
+  }
+  catch {
+    return 1;
+  }
+};
 
 export {
   zeroPad,
-  dummy,
+  getProductData,
+  getStyleData,
+  getProductId,
 };
