51fda427f39c16826c3634ba2cbb3f60efb8c9ab
han-yan-ds <hanprogramming@gmail.com>
2019-09-03 17:26:17 -0600

    Merged with master

--- src/store.js
+++ src/store.js
@@ -10,7 +10,7 @@ import exampleProductData from './exampleProductData';
 const initialData = {
   productData: exampleProductData,
   styleData: exampleStyleData,
-  productId: 54,
+  productId: 18,
 };
 
 export default function configureStore(initialState = initialData) {
