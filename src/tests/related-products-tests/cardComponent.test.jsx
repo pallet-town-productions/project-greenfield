9d619d5aefca0280cec07faa8a2ab475dbb4c4fc
jbc5099 <jbc5099@gmail.com>
2019-09-04 14:27:02 -0600

    testing

--- src/tests/related-products-tests/cardComponent.test.jsx
+++ src/tests/related-products-tests/cardComponent.test.jsx
@@ -4,13 +4,13 @@ import Adapter from 'enzyme-adapter-react-16';
 import { RelatedCard } from '../../components/related-Products-components/relatedCard';
 
 Enzyme.configure({ adapter: new Adapter() });
-jest.mock('../../util/api', () => 'http://12.345.678.9101:3000');
+
 
 function setup() {
   const props = {
     productId: 1,
   };
-
+  jest.mock('../../util/api', () => 'http://12.345.678.9101:3000');
   const enzymeWrapper = shallow(<RelatedCard productId={1} />);
 
   return {
