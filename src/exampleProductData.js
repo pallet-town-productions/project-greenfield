5fc8b65fa6fb8ded3432bf5960bdc1e2462ca498
han-yan-ds <hanprogramming@gmail.com>
2019-09-04 12:27:54 -0600

    Modified store to allow a testing mode and a regular mode, to allow tests to run

--- src/exampleProductData.js
+++ src/exampleProductData.js
@@ -1,6 +1,6 @@
 const exampleProductData = {
-  id: -999,
-  name: 'Dummy Onesie',
+  id: 1,
+  name: 'Camo Onesie',
   slogan: 'Blend in to your crowd',
   description: 'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
   category: 'Jackets',
